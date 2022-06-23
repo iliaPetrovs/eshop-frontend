887d858cee3cb359b250ecd124a6527b3db29e70
iliaPetrovs <ilja.petrovs@transferwise.com>
2022-06-23 15:00:00 +0100

    edit  product modal changes

--- src/components/Admin/EditProductForm.js
+++ src/components/Admin/EditProductForm.js
@@ -1,129 +1,129 @@
-import axios from "axios";
-import React, { useLayoutEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useLayoutEffect, useState } from "react";
 
-export default function EditProductForm() {
-    const initialState = {
-        name: "",
-        description: "",
-        price: "",
-        category: "",
-        imageUrl: "",
-        stock: "",
-    };
+// export default function EditProductForm() {
+//     const initialState = {
+//         name: "",
+//         description: "",
+//         price: "",
+//         category: "",
+//         imageUrl: "",
+//         stock: "",
+//     };
 
-    const [state, setState] = useState(initialState);
+//     const [state, setState] = useState(initialState);
 
-    useLayoutEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+//     useLayoutEffect(() => {
+//         window.scrollTo(0, 0);
+//     }, []);
 
-    const categories = ["Choose category...", "Bag", "Patch", "Sticker", "Pin"];
+//     const categories = ["Choose category...", "Bag", "Patch", "Sticker", "Pin"];
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
+//     const handleOnSubmit = (e) => {
+//         e.preventDefault();
 
-        let bodyFormData = new FormData();
-        bodyFormData.append("name", state.name);
-        bodyFormData.append("description", state.description);
-        bodyFormData.append("price", state.price);
-        bodyFormData.append("category", state.category);
-        bodyFormData.append("imageUrl", state.imageUrl);
-        bodyFormData.append("stock", state.stock);
+//         let bodyFormData = new FormData();
+//         bodyFormData.append("name", state.name);
+//         bodyFormData.append("description", state.description);
+//         bodyFormData.append("price", state.price);
+//         bodyFormData.append("category", state.category);
+//         bodyFormData.append("imageUrl", state.imageUrl);
+//         bodyFormData.append("stock", state.stock);
 
-        axios
-            .post("http://localhost:8080/products", bodyFormData)
-            .then(() => alert("SUCCESS"))
-            .then(() => {
-                setState(initialState);
-            })
-            .catch((e) => console.log(e));
-    };
+//         axios
+//             .post("http://localhost:8080/products", bodyFormData)
+//             .then(() => alert("SUCCESS"))
+//             .then(() => {
+//                 setState(initialState);
+//             })
+//             .catch((e) => console.log(e));
+//     };
 
-    return (
-        <div className="inline-edit-container">
-            <form>
-                <td>
-                <input
-                    className="add-input-field"
-                    placeholder="Name"
-                    value={state.name}
-                    onChange={(e) =>
-                        setState({ ...state, name: e.target.value })
-                    }
-                    required
-                ></input>
-                </td>
-                <td>
-                <input
-                    className="add-input-field"
-                    value={state.description}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            description: e.target.value,
-                        })
-                    }
-                    required
-                    placeholder="Description"
-                ></input>
-                </td>
-                <td>
-                <input
-                    className="add-input-field"
-                    value={state.description}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            description: e.target.value,
-                        })
-                    }
-                    required
-                    placeholder="Description"
-                ></input>
-                </td>
-                <td>
-                <input
-                    className="add-input-field"
-                    value={state.description}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            description: e.target.value,
-                        })
-                    }
-                    required
-                    placeholder="Description"
-                ></input>
-                </td>
-                <td>
-                <input
-                    className="add-input-field"
-                    value={state.description}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            description: e.target.value,
-                        })
-                    }
-                    required
-                    placeholder="Description"
-                ></input>
-                </td>
-                <td>
-                <input
-                    className="add-input-field"
-                    value={state.description}
-                    onChange={(e) =>
-                        setState({
-                            ...state,
-                            description: e.target.value,
-                        })
-                    }
-                    required
-                    placeholder="Description"
-                ></input>
-                </td>
-            </form>
-        </div>
-    );
-}
+//     return (
+//         <div className="inline-edit-container">
+//             <form>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     placeholder="Name"
+//                     value={state.name}
+//                     onChange={(e) =>
+//                         setState({ ...state, name: e.target.value })
+//                     }
+//                     required
+//                 ></input>
+//                 </td>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     value={state.description}
+//                     onChange={(e) =>
+//                         setState({
+//                             ...state,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                     required
+//                     placeholder="Description"
+//                 ></input>
+//                 </td>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     value={state.description}
+//                     onChange={(e) =>
+//                         setState({
+//                             ...state,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                     required
+//                     placeholder="Description"
+//                 ></input>
+//                 </td>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     value={state.description}
+//                     onChange={(e) =>
+//                         setState({
+//                             ...state,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                     required
+//                     placeholder="Description"
+//                 ></input>
+//                 </td>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     value={state.description}
+//                     onChange={(e) =>
+//                         setState({
+//                             ...state,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                     required
+//                     placeholder="Description"
+//                 ></input>
+//                 </td>
+//                 <td>
+//                 <input
+//                     className="add-input-field"
+//                     value={state.description}
+//                     onChange={(e) =>
+//                         setState({
+//                             ...state,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                     required
+//                     placeholder="Description"
+//                 ></input>
+//                 </td>
+//             </form>
+//         </div>
+//     );
+// }
--- src/components/Admin/ProductDashboard.js
+++ src/components/Admin/ProductDashboard.js
@@ -1,14 +1,17 @@
 import React, { useState, useMemo } from "react";
 import axios from "axios";
-import {} from "@material-ui/core";
+import { Modal } from "@material-ui/core";
 import { Card, Table, Image, ButtonGroup, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { toCurrency } from "../../utils/currency";
-import EditProductForm from "./EditProductForm";
+import classNames from "classnames";
+// import EditProductForm from "./EditProductForm";
 
 export default function ProductDashboard() {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [itemToEdit, setItemToEdit] = useState({});
 
     useMemo(() => {
         axios
@@ -27,6 +30,15 @@ export default function ProductDashboard() {
     const handleDelete = (itemId) => {
         axios.delete(`http://localhost:8080/products/${itemId}`);
         setItems(items.filter((item) => item.id !== itemId));
+    };
+
+    const handleEdit = (item) => {
+        setItemToEdit(item);
+        setModalOpen(true);
+    };
+
+    const handleClose = () => {
+        setItemToEdit({});
     };
 
     return (
@@ -75,12 +87,15 @@ export default function ProductDashboard() {
                                         </td>
                                         <td>
                                             <ButtonGroup>
-                                                <Link
-                                                    to={"edit/" + item.id}
-                                                    className="btn btn-sm btn-outline-primary"
+                                                <Button
+                                                    size="sm"
+                                                    variant="outline-success"
+                                                    onClick={() =>
+                                                        handleEdit(item)
+                                                    }
                                                 >
                                                     Edit
-                                                </Link>
+                                                </Button>
                                                 <Button
                                                     size="sm"
                                                     variant="outline-danger"
@@ -93,63 +108,188 @@ export default function ProductDashboard() {
                                             </ButtonGroup>
                                         </td>
                                     </tr>
-                                    <tr key={item.id}>
-                                      <form>
-                                        <td className="thumbnail">
-                                            <Image
-                                                src={item.imageUrl}
-                                                roundedCircle
-                                                width="25"
-                                                height="25"
-                                            />
-                                            {item.title}
-                                        </td>
-                                        <td>
-                                            <input
-                                                className="add-input-field"
-                                                placeholder="Name"
-                                                required
-                                            ></input>
-                                        </td>
-                                        <td className="toggle-category">
-                                            {item.description}
-                                        </td>
-                                        <td className="toggle-category">
-                                            {toCurrency(item.price)}
-                                        </td>
-                                        <td className="toggle-category">
-                                            {item.category}
-                                        </td>
-                                        <td className="toggle-category">
-                                            {item.stock}
-                                        </td>
-                                        <td>
-                                            <ButtonGroup>
-                                                <Link
-                                                    to={"edit/" + item.id}
-                                                    className="btn btn-sm btn-outline-primary"
-                                                >
-                                                    Edit
-                                                </Link>
-                                                <Button
-                                                    size="sm"
-                                                    variant="outline-danger"
-                                                    onClick={() =>
-                                                        handleDelete(item.id)
-                                                    }
-                                                >
-                                                    Bin
-                                                </Button>
-                                            </ButtonGroup>
-                                        </td>
-                                        </form>
-                                    </tr>
-                                    {/* <EditProductForm /> */}
                                 </>
                             ))}
                     </tbody>
                 </table>
             </div>
+            <Modal
+                open={modalOpen}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <EditProductForm
+                    setModalOpen={setModalOpen}
+                    item={itemToEdit}
+                />
+            </Modal>
         </div>
+    );
+}
+
+function EditProductForm({ setModalOpen, item }) {
+    const initialState = {
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        category: item.category,
+        imageUrl: item.imageUrl,
+        stock: item.stock,
+    };
+    const [state, setState] = useState(initialState);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const categories = ["Choose category...", "Bag", "Patch", "Sticker", "Pin"];
+
+    console.log(item);
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        let bodyFormData = new FormData();
+        bodyFormData.append("id", item.id);
+        bodyFormData.append("name", state.name);
+        bodyFormData.append("description", state.description);
+        bodyFormData.append("price", state.price);
+        bodyFormData.append("category", state.category);
+        bodyFormData.append("imageUrl", state.imageUrl);
+        bodyFormData.append("stock", state.stock);
+
+        axios
+            .put("http://localhost:8080/products", bodyFormData)
+            .then(() => {
+                setModalOpen(false);
+                window.location.reload();
+            })
+            .catch((e) => console.log(e));
+    };
+
+    return (
+        <>
+            <div style={{ paddingTop: "140px" }}></div>
+            <div className="add-product-container">
+                <h2 className="add-product-header">Edit Product</h2>
+                <button
+                    className="close-modal"
+                    onClick={() => setModalOpen(false)}
+                >
+                    <i class="fa-solid fa-square-xmark"></i>
+                </button>
+                <div className="add-form-container d-flex">
+                    <form onSubmit={handleOnSubmit} className="add-form d-flex">
+                        <div className="edit-product-image">
+                            <img
+                                // style={{ width: "200px" }}
+                                src={item.imageUrl}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                className="add-input-field"
+                                placeholder="Name"
+                                value={state.name}
+                                onChange={(e) =>
+                                    setState({ ...state, name: e.target.value })
+                                }
+                                required
+                            ></input>
+                            <input
+                                className="add-input-field"
+                                value={state.description}
+                                onChange={(e) =>
+                                    setState({
+                                        ...state,
+                                        description: e.target.value,
+                                    })
+                                }
+                                required
+                                placeholder="Description"
+                            ></input>
+                            <select
+                                id="category"
+                                className="custom-select"
+                                onChange={(e) =>
+                                    setState({
+                                        ...state,
+                                        category: e.target.value,
+                                    })
+                                }
+                                required
+                            >
+                                {categories.map((category) => {
+                                    return (
+                                        <option value={category.toLowerCase()}>
+                                            {category}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                            <input
+                                className="add-input-field"
+                                value={state.imageUrl}
+                                onChange={(e) =>
+                                    setState({
+                                        ...state,
+                                        imageUrl: e.target.value,
+                                    })
+                                }
+                                required
+                                placeholder="Image URL"
+                            ></input>
+                            <input
+                                className="add-input-field"
+                                value={state.price}
+                                type="number"
+                                onChange={(e) =>
+                                    setState({
+                                        ...state,
+                                        price: e.target.value,
+                                    })
+                                }
+                                required
+                                placeholder="Price"
+                            ></input>
+                            <input
+                                className="add-input-field"
+                                value={state.stock}
+                                type="number"
+                                onChange={(e) =>
+                                    setState({
+                                        ...state,
+                                        stock: e.target.value,
+                                    })
+                                }
+                                required
+                                placeholder="Stock"
+                            ></input>
+                            <div className="mb-3 p-4">
+                                <button
+                                    type="submit"
+                                    className="pay-btn-container"
+                                >
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div
+                className={classNames("alert-wrapper", {
+                    "alert-hidden": !showAlert,
+                })}
+            >
+                <div className="alert-container">
+                    <div className="alert-box">
+                        <span className="info-circle-container"></span>
+                        <i class="fa-solid fa-circle-info info-circle"></i>
+                        <span className="alert-content">
+                            Product added successfully
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
