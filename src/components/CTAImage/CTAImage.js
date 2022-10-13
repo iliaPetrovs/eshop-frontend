e0834d31cba2417daf7f298c26768c707c1f8c28
iliaPetrovs <ilja.petrovs@transferwise.com>
2022-10-13 12:40:57 +0100

    added cta image component

---
+++ src/components/CTAImage/CTAImage.js
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
+
+import styles from "./CTAImage.module.css";
+
+export default function CTAImage({ title, subtitle, body, link, image }) {
+    return (
+        <div className={classNames("", styles.container)}>
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <div className={styles.image}>
+                        <img src={image} alt="" />
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 d-flex">
+                    <div className={styles.content}>
+                        <div className={styles.headers}>
+                            <h3>{subtitle}</h3>
+                            <h1>{title}</h1>
+                        </div>
+                        <p>{body}</p>
+                        <div className={styles.cta}>
+                            <Link to={link}>
+                                <Button value="Click here" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+CTAImage.defaultProps = {
+    title: "Title",
+    subtitle: "Subtitle",
+    body: "Body",
+    link: "/hello",
+    image: "https://i.etsystatic.com/22179317/r/il/c593b0/4233377459/il_680x540.4233377459_jyfc.jpg",
+};
