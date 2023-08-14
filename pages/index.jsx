52067f3c285ae164ad3013ddbc40a23f9559526a
Ilja Petrovs <ilja.petrovs@transferwise.com>
2023-08-15 00:15:31 +0100

    test backend

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,16 +4,32 @@ import { getProducts } from '../src/api/getProducts'
 import Home from '../src/components/Home'
 import Layout from '../src/components/Layout/Layout'
 import items from '../src/utils/mocks/items'
+import axios from "axios";
 
-function HomePage () {
-  const products = useQuery({ queryKey: ['products'], queryFn: getProducts })
-  return (
-    <>
-      <Layout>
-        <Home products={{ data: items }} />
-      </Layout>
-    </>
-  )
+function HomePage(props) {
+    const products = useQuery({ queryKey: ["products"], queryFn: getProducts });
+    console.log(props.test);
+    return (
+        <>
+            <Layout>
+                <Home products={{ data: items }} />
+            </Layout>
+        </>
+    );
 }
+
+export const getServerSideProps = async () => {
+    const res = await axios.get("http://localhost:8080/api/", {
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    });
+    return {
+        props: {
+            test: res.data,
+        },
+    };
+};
 
 export default HomePage
